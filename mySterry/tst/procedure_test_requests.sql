e7e94009894ddfd337018227c4d22437801cd038
[ progorker ] Software Development as a Platform <zinospetrel@sdf.org>
2026-06-04 19:03:08 +0700

    Add files via upload

--- mySterry/tst/procedure_test_requests.sql
+++ mySterry/tst/procedure_test_requests.sql
@@ -33,7 +33,7 @@ begin
 
   call mytestorproxy.api_testor_suite_case( p_token, v_suite_id, v_case_id, p_suite_code, 'test_requests' );
 
-  set v_request = concat( '192.168.89.203\r\n8311\r\nAZDKOGOEWGDAH\r\n', sterry_escape( '{\"n\":2}' ), '\r\n/testor/\r\n\r\n' );
+  set v_request = concat( '192.168.89.203\r\n8311\r\nAZDKOGOEWGDAH\r\n{\"n\":2}\r\n/testor/\r\n\r\n' );
   set v_sterry_host = '';
   set v_sterry_port = -1;
   set v_sterry_path = '';
@@ -41,12 +41,11 @@ begin
   set v_sterry_json_data = '';
 
   call sterry_parse_request( v_request, v_sterry_host, v_sterry_port, v_sterry_auth_token, v_sterry_json_data, v_sterry_path );
-  set v_json = trim( left( v_sterry_json_data, 8192 ) );
 
   call mytestorproxy.api_testor_same( p_token, v_test_id, v_suite_id, v_case_id, 'host.1', v_sterry_host, '192.168.89.203' );
   call mytestorproxy.api_testor_equals( p_token, v_test_id, v_suite_id, v_case_id, 'port.1', v_sterry_port, 8311 );
   call mytestorproxy.api_testor_same( p_token, v_test_id, v_suite_id, v_case_id, 'auth_token.1', v_sterry_auth_token, 'AZDKOGOEWGDAH' );
-  call mytestorproxy.api_testor_same( p_token, v_test_id, v_suite_id, v_case_id, 'json_data.1', sterry_escape( v_json ), sterry_escape( '{\"n\":2}' ) );
+  call mytestorproxy.api_testor_same( p_token, v_test_id, v_suite_id, v_case_id, 'json_data.1', v_sterry_json_data, '{\"n\":2}' );
   call mytestorproxy.api_testor_same( p_token, v_test_id, v_suite_id, v_case_id, 'path.1', v_sterry_path, '/testor/' );
 end;$$
 delimiter ;
